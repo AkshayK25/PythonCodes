3d0302279aefddbcf5cda6a9df8713994a4f899b
akshay <aman.akshay88@gmail.com>
2018-06-06 11:19:27 +0530

    Akshay

---
+++ Akshay.py
@@ -0,0 +1,31 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Name:: %s ,Salary:: %d" % ("Tony Stark",1000000))
+Name:: Tony Stark ,Salary:: 1000000
+>>> print("student: of {0:s} opting {1:s} paid fees {2:s}".format("Acadview","Python","5000"))
+student: of Acadview opting Python paid fees 5000
+>>> type = input()
+Let's get started
+>>> print(type)
+Let's get started
+>>> x = input("Enter your name here :")
+Enter your name here :Akshay
+>>> y = input("Enter your Age -> ")
+Enter your Age -> 21 years
+>>> z = input("Enter Your known programming language : ")
+Enter Your known programming language : c++
+>>> print(x)
+Akshay
+>>> print(y)
+21 years
+>>> print(z)
+c++
+>>> a = input()
+Acad
+>>> b = input()
+View
+>>> print(a+b)
+AcadView
+>>> print("Welcome to the World of Python ")
+Welcome to the World of Python 
+>>> 
